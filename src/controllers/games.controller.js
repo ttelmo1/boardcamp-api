8313c9c1c72df3837a8b26ef0035af1a38e2aaca
Telmo Junior <telmobsj@gmail.com>
2023-02-09 19:50:07 -0300

    rota post

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -11,3 +11,25 @@ export async function getGames(req, res) {
         console.log(err);
     }
 }
+
+export async function postGame(req, res) {
+    const { name, image, stockTotal, pricePerDay } = req.body;
+    try {
+      const gameExists = await db.query('SELECT * FROM games WHERE name = $1', [
+        name,
+      ]);
+      if (gameExists.rowCount > 0) {
+        return res.sendStatus(409);
+      }
+      const result = await db.query(
+        'INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4)',
+        [name, image, stockTotal, pricePerDay]
+      );
+      if (result.rowCount === 0) {
+        return res.sendStatus(400);
+      }
+      res.sendStatus(201);
+    } catch (error) {
+      res.sendStatus(500);
+    }
+  }
--- src/routers/games.route.js
+++ src/routers/games.route.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { getGames } from "../controllers/games.controller.js";
+import { getGames, postGame } from "../controllers/games.controller.js";
+import { validateSchema } from "../middlewares/validate.middleware.js";
+import { gameSchema } from "../schemas/game.schema.js";
 
 const games = Router();
 
 games.get("/", getGames);
+games.post("/", validateSchema(gameSchema), postGame);
 
 export default games;
