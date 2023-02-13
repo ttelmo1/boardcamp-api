1f7a5f3ad057871751247605ce57b60839943140
Telmo Junior <telmobsj@gmail.com>
2023-02-13 09:51:48 -0300

    feat: delete rental

--- src/routers/rentals.routes.js
+++ src/routers/rentals.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { finishRental, getRentals, postRentals } from "../controllers/rentals.controller.js";
+import { deleteRental, finishRental, getRentals, postRentals } from "../controllers/rentals.controller.js";
 import { validateSchema } from "../middlewares/validate.middleware.js";
 import { postRentalSchema } from "../schemas/rental.schema.js";
 
@@ -9,5 +9,6 @@ const rental = Router();
 rental.get("/", getRentals);
 rental.post("/", validateSchema(postRentalSchema), postRentals);
 rental.post("/:id/return", finishRental);
+rental.delete("/id", deleteRental)
 
 export default rental;
